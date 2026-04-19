2ba82ed275b3a7525673362b749a28ee7d2b74bd
Marc Baqué <marc.baque@factorial.co>
2026-04-19 18:34:04 +0200

    chore: remove debug E2E spec

--- e2e/debug.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from './helpers/app'
-
-test('app launches and shows root element', async ({ page }) => {
-  expect(page).toBeTruthy()
-  const html = await page.content()
-  console.log('PAGE HTML (first 500):', html.slice(0, 500))
-})
